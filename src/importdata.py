70696ce4884d04d4c99f5c7692b3b324ccf41306
Rick Leir <rleir@leirtech.com>
2019-01-04 13:44:41 -0500

    added chart_by_site

--- src/importdata.py
+++ src/importdata.py
@@ -13,40 +13,82 @@ __status__ = "Production"
 import csv
 import json
 
-input_file = 'four_org.csv'
+def chart_by_year():
+    input_file = 'four_org.csv'
 
-# read csv
-with open(input_file, newline='') as csvfile:
-
+    output_file = 'chartByYear.json'
     all_data   = []
-    input_data = csv.DictReader(csvfile)
-    for row in input_data:
-        #build data struc
-        keys = list(row.keys())
-        years = keys[3:]
 
-        organism_counts = []
-        for year in years:
-            if row[year] == "":
-                organism_counts.append( [year, "Number.NaN"])
-            else:
-                organism_counts.append( [year, row[year]])
+    # read input
+    with open(input_file, newline='') as csvfile:
 
-        organism_name_and_site = row['organism'] + " / "  + row['site']
-        if "organism" in row:
-            del row["organism"]
-        if "site" in row:
-            del row["site"]
-        if "maxAvgAll" in row:
-            del row["maxAvgAll"]
+        input_data = csv.DictReader(csvfile)
+        for row in input_data:
+            #build data struc
+            keys = list(row.keys())
+            years = keys[3:]
 
-        organism_data   = {}
-        organism_data["key"] = organism_name_and_site
-        organism_data["values"] = organism_counts
-        all_data.append( organism_data )
+            organism_counts = []
+            for year in years:
+                if row[year] == "":
+                    organism_counts.append( [year, "Number.NaN"])
+                else:
+                    organism_counts.append( [year, row[year]])
+
+            organism_name_and_site = row['organism'] + " / "  + row['site']
+            if "organism" in row:
+                del row["organism"]
+                if "site" in row:
+                    del row["site"]
+                if "maxAvgAll" in row:
+                    del row["maxAvgAll"]
+
+            organism_data   = {}
+            organism_data["key"] = organism_name_and_site
+            organism_data["values"] = organism_counts
+            all_data.append( organism_data )
         
-    # write json
-    with open("chart.json", "w") as fp:
+    # write output
+    with open(output_file, "w") as fp:
         json.dump(all_data , fp) 
 
 
+def chart_by_site():
+    input_file = 'three_sites.csv'
+
+    output_file = 'chartBySite.json'
+    all_data   = []
+
+    # read input
+    with open(input_file, newline='') as csvfile:
+
+        input_data = csv.DictReader(csvfile)
+        for row in input_data:
+            #build data struc
+            keys = list(row.keys())
+            sites = keys[1:]
+
+            organism_counts = []
+            site_num = 0
+            for site in sites:
+                site_num += 1
+                if row[site] == "":
+                    organism_counts.append( [site_num, "Number.NaN"])
+                else:
+                    organism_counts.append( [site_num, row[site]])
+
+            organism_name = row['organism']
+            organism_data   = {}
+            organism_data["key"] = organism_name
+            organism_data["values"] = organism_counts
+            all_data.append( organism_data )
+            
+    # write output
+    with open(output_file, "w") as fp:
+        json.dump(all_data , fp) 
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    chart_by_year()
+    chart_by_site()
